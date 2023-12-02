d9194e72811c71e714eff0f805855fd84704decd
yuskey38 <yskmyt.dev@gmail.com>
2023-12-02 22:48:30 +0900

    refine

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script>
+	import 'bootstrap/dist/css/bootstrap.min.css';
+	let isOpen = false;
+	function toggleOpen() {
+		isOpen = !isOpen;
+	}
+</script>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/">yuskey.com</a>
+		<button
+			on:click={toggleOpen}
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarNav"
+			aria-controls="navbarNav"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class:show={isOpen} class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="/">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/about">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/contact">Contact</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<main class="container">
+	<slot />
+</main>
