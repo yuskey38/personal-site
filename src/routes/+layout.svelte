47e3746e273d04fd110c952512a1fd75b191d7bc
yuskey38 <yskmyt.dev@gmail.com>
2023-12-03 16:40:24 +0900

    add form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -32,9 +32,6 @@
 				<li class="nav-item">
 					<a class="nav-link" href="/contact">Contact</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" aria-disabled="true">Disabled</a>
-				</li>
 			</ul>
 		</div>
 	</div>
